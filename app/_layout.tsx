6b1ad004078614aba7cc0fcaef79eed61c0cf796
vitor367 <vp006949@cfjl.com.br>
2025-08-09 18:47:37 -0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,7 @@
 import { Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { Platform } from 'react-native';
-import HashRouter from '../components/HashRouter';
 
 export default function RootLayout() {
- useEffect(() => {
-   if (Platform.OS === 'web') {
-     // Handle initial hash routing
-     const hash = window.location.hash;
-     if (hash && hash.length > 1) {
-       const route = hash.substring(1);
-       import('expo-router').then(({ router }) => {
-         setTimeout(() => router.replace(route as any), 100);
-       });
-     }
-   }
- }, []);
-
- return (
-   <>
-     <HashRouter />
-     <Stack screenOptions={{ headerShown: false }} />
-   </>
- );
+  return (
+    <Stack screenOptions={{ headerShown: false }} />
+  );
 }
